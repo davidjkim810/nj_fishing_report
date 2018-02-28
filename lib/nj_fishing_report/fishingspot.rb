abca5e56d6bcea241cb7f57e88df7828849034e8
github username <github email address>
2018-02-28 16:46:48 -0500

    scraper creates new instances of objects of nj fishing locations

--- lib/nj_fishing_report/fishingspot.rb
+++ lib/nj_fishing_report/fishingspot.rb
@@ -2,11 +2,19 @@
 
 class NjFishingReport::FishingSpot
 
-  attr_accessor :name, :tide, :weather
+  attr_accessor :name, :tide, :weather, :report
   @@all = []
-  
+
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
 end
--- lib/nj_fishing_report/scraper.rb
+++ lib/nj_fishing_report/scraper.rb
@@ -1,22 +1,24 @@
 
 class NjFishingReport::Scraper
 
-  def self.scrape_fishing_locations(location_url)
+  def self.scrape_fishing_locations
     doc =
     Nokogiri::HTML(open('http://www.fishingreportsnow.com/New_Jersey_Fishing_Reports_Pro.cfm/reg/7'))
 
     fishing_locations = []
     doc.css("td.reportstext a").each do |potential_location|
-      if potential_location.attribute("href").value[0,30] == "New_Jersey_Fishing_Reports_Pro"
+      if potential_location.attribute("href").value[0,30] == "New_Jersey_Fishing_Reports_Pro" && potential_location.text != "Last Week's Report"
         fishing_locations << potential_location
       end
     end
 
     fishing_locations.each do |location|
-
+      NjFishingReport::FishingSpot.new(location.text)
+    end
   end
 
-  def self.scrape_fishing_report()
+binding.pry
+  def self.scrape_fishing_report
   end
 
 end
